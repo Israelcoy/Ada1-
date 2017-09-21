2df1a26c31d77e99f481fb0898094dbe7892c9c4
Israelcoy <asricb@gmail.com>
2017-09-21 13:18:30 +0200

    Escena 1 Terminada

---
+++ Ada1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * Time.deltaTime * 1, Space.World);
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.tag == "Killer") {
+			Destroy (this.gameObject);
+		}
+	} 
+}
